bf563f32dd2dee738f67d69b95074b2b5d3bae6a
lucag316 <lucafranco316@gmail.com>
2024-05-08 12:46:22 -0300

    Actualizado

--- Gargiulo.Luca.PrimerParcialLabo2/WindowsForm/FormPrincipal.cs
+++ Gargiulo.Luca.PrimerParcialLabo2/WindowsForm/FormPrincipal.cs
@@ -1,8 +1,8 @@
 namespace WindowsForm
 {
-    public partial class Form1 : Form
+    public partial class FormPrincipal : Form
     {
-        public Form1()
+        public FormPrincipal()
         {
             InitializeComponent();
         }
