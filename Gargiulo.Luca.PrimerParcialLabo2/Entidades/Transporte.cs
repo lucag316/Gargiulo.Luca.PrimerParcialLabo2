d2c2c6a695061c78b996ecd50deba4002a4bc03a
lucag316 <lucafranco316@gmail.com>
2024-05-08 11:39:08 -0300

    Actualizado

--- Gargiulo.Luca.PrimerParcialLabo2/Entidades/Transporte.cs
+++ Gargiulo.Luca.PrimerParcialLabo2/Entidades/Transporte.cs
@@ -1,14 +1,25 @@
-﻿namespace Entidades
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Entidades
 {
     public class Transporte
     {
         #region Atributos
+        private int velocidad;
+        private int capacidadPasajeros;
+        private int cargaMaxima;
         #endregion
 
         #region Propiedades
         #endregion
 
         #region Constructor
+        public Transporte(int velocidad, int capacidadPasajeros, int cargaMaxima)
+        {
+            this.velocidad = velocidad;
+            this.capacidadPasajeros = capacidadPasajeros;
+            this.cargaMaxima = cargaMaxima;
+        }
         #endregion
 
         #region Metodos
