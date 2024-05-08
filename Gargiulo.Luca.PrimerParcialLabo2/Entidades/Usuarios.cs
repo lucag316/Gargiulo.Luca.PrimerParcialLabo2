a72a1f19bdac4e1dd64baf7a1bbcbc4be5f4ac3e
lucag316 <lucafranco316@gmail.com>
2024-05-08 15:58:22 -0300

    Actualizado

---
+++ Gargiulo.Luca.PrimerParcialLabo2/Entidades/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal class Usuarios
+    {
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public int Legajo { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public string Perfil { get; set; }
+
+    }
+}
--- Gargiulo.Luca.PrimerParcialLabo2/WindowsForm/FrmLogin.cs
+++ Gargiulo.Luca.PrimerParcialLabo2/WindowsForm/FrmLogin.cs
@@ -13,7 +13,7 @@ namespace WindowsForm
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
-        {
+        {//CREO QUE NI HACE FALTA SI TIRO EL MENSAJE DE ERROR, PERO NO SE QUE FORMA ES MEJOR
             txtCorreo.Clear();//limpio los text box
             txtClave.Clear();
         }
@@ -22,13 +22,14 @@ namespace WindowsForm
         {
             if (txtCorreo.Text == "luca" && txtClave.Text == "123")
             {
-                FrmLogin frmPrincipal = new FrmLogin();
-                //this.Hide();//que hace?
+                FrmPrincipal frmPrincipal = new FrmPrincipal();
+                //frmPrincipal.StartPosition = FormStartPosition.CenterScreen;//aca no es, ya probe
+                this.Hide();//lo oculta, si no lo pongo queda atras del frmPrincipal?
                 frmPrincipal.Show();
             }
             else
             {
-                MessageBox.Show("Datos Incorrectos", "ERROR");
+                MessageBox.Show("Datos Incorrectos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 txtCorreo.Clear();
                 txtClave.Clear();
             }
