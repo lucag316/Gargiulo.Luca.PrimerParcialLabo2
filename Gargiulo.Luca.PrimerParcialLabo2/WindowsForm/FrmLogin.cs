2f62fc6fe5f1bdbd3a398516b054cebd650ec0ca
lucag316 <lucafranco316@gmail.com>
2024-05-09 15:47:20 -0300

    Actualizando probando JSON

--- Gargiulo.Luca.PrimerParcialLabo2/WindowsForm/FrmLogin.cs
+++ Gargiulo.Luca.PrimerParcialLabo2/WindowsForm/FrmLogin.cs
@@ -11,7 +11,8 @@ namespace WindowsForm
         public FrmLogin()
         {
             InitializeComponent();
-        }
+            this.usuarios = DeserializarUsuariosJSON("usuarios.json");
+        }//C:\\Users\\luca_\\Desktop\\Labo2 primerParcial\\Gargiulo.Luca.PrimerParcialLabo2\\Gargiulo.Luca.PrimerParcialLabo2\\usuarios.json
 
         private void FrmLogin_Load(object sender, EventArgs e)
         {
@@ -26,12 +27,15 @@ namespace WindowsForm
 
         private void btnIniciarSesion_Click(object sender, EventArgs e)
         {
-            if (txtCorreo.Text == "luca" && txtClave.Text == "123")
+            bool usuarioValidado = VerificarUsuario(txtCorreo.Text, txtClave.Text);
+
+            if (usuarioValidado)
             {
                 FrmPrincipal frmPrincipal = new FrmPrincipal();
                 //frmPrincipal.StartPosition = FormStartPosition.CenterScreen;//aca no es, ya probe
                 this.Hide();//lo oculta, si no lo pongo queda atras del frmPrincipal?
                 frmPrincipal.Show();
+                MessageBox.Show("Inicio de sesion exitoso");
             }
             else
             {
@@ -41,14 +45,30 @@ namespace WindowsForm
             }
         }
 
-        public void DeserializarJSON()
+        private List<Usuario> DeserializarUsuariosJSON(string pathArchivo)
         {
-            using (StreamReader streamReader = new StreamReader("./usuarios.json"))
+            using (StreamReader streamReader = new StreamReader(pathArchivo))
             {
                 string jsonUsuarios = streamReader.ReadToEnd();
 
                 this.usuarios = JsonSerializer.Deserialize<List<Usuario>>(jsonUsuarios);
             }
+            return this.usuarios;
+        }
+
+        private bool VerificarUsuario(string correo, string clave)
+        {
+            bool estaElUsuario = false;
+
+            foreach (Usuario usuario in this.usuarios)
+            {
+                if(usuario.Correo == correo && usuario.Clave == clave)
+                {
+                    estaElUsuario = true;
+                    break; //aca creo que va break para que cuando lo encuentre para de iterar
+                }
+            }
+            return estaElUsuario;
         }
 
     }
