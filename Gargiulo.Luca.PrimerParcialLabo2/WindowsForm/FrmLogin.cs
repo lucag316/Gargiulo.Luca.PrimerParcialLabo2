5da4fd9563d556fb927fcf061213895c374bd189
lucag316 <lucafranco316@gmail.com>
2024-05-09 17:22:46 -0300

    Actualizando login

--- Gargiulo.Luca.PrimerParcialLabo2/WindowsForm/FrmLogin.cs
+++ Gargiulo.Luca.PrimerParcialLabo2/WindowsForm/FrmLogin.cs
@@ -1,35 +1,101 @@
 using Entidades;
 using System.Text.Json;
-using System.Xml.Serialization;
+using System.Text.Json.Serialization;
+
 
 namespace WindowsForm
 {
     public partial class FrmLogin : Form
     {
         private List<Usuario> usuarios;
+        public int intentos = 0;
 
+        private string pathJsonUsuarios = "C:\\Users\\luca_\\Desktop\\Labo2 primerParcial\\Gargiulo.Luca.PrimerParcialLabo2\\Gargiulo.Luca.PrimerParcialLabo2\\WindowsForm\\usuarios.json";
         public FrmLogin()
         {
             InitializeComponent();
-            this.usuarios = DeserializarUsuariosJSON("usuarios.json");
-        }//C:\\Users\\luca_\\Desktop\\Labo2 primerParcial\\Gargiulo.Luca.PrimerParcialLabo2\\Gargiulo.Luca.PrimerParcialLabo2\\usuarios.json
+            this.CenterToScreen();
+            this.usuarios = DeserializarUsuariosJSON(this.pathJsonUsuarios);
+        }
 
         private void FrmLogin_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void btnCancelar_Click(object sender, EventArgs e)
-        {//CREO QUE NI HACE FALTA SI TIRO EL MENSAJE DE ERROR, PERO NO SE QUE FORMA ES MEJOR
-            txtCorreo.Clear();//limpio los text box
-            txtClave.Clear();
-        }
 
         private void btnIniciarSesion_Click(object sender, EventArgs e)
         {
+            //List<Usuario> listaUsuarios = DeserializarUsuariosJSON(this.pathJsonUsuarios);
             bool usuarioValidado = VerificarUsuario(txtCorreo.Text, txtClave.Text);
 
             if (usuarioValidado)
+            {
+                FrmPrincipal frmPrincipal = new FrmPrincipal();
+                frmPrincipal.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Datos Incorrectos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtCorreo.Clear();
+                txtClave.Clear();
+                this.intentos++;
+            }
+
+
+
+            /*if (this.intentos <= 3)
+            {
+                if (this.usuarios != null)
+                {
+                    bool usuarioValidado = false;
+
+
+
+                    foreach (Usuario usuario in this.usuarios)
+                    {
+                        if (txtCorreo.Text == usuario.Correo && txtClave.Text == usuario.Clave)//tratar de hacerlo con verificarUsuario
+                        {
+                            usuarioValidado = true;
+                            break;
+                        }
+                        
+                    }
+                    if (usuarioValidado)
+                    {
+                        FrmPrincipal frmPrincipal = new FrmPrincipal();
+                        frmPrincipal.Show();
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Datos Incorrectos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtCorreo.Clear();
+                        txtClave.Clear();
+                        this.intentos++;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error al cargar usuarios.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //mensaje de a pasado el numero de intentos permitidos
+                    //this.Close();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Ha excedido el número de intentos permitidos.", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close(); // Otra acción en caso de exceder el número de intentos
+            }*/
+
+
+
+
+
+
+            /*if (usuarioValidado)
             {
                 FrmPrincipal frmPrincipal = new FrmPrincipal();
                 //frmPrincipal.StartPosition = FormStartPosition.CenterScreen;//aca no es, ya probe
@@ -42,18 +108,21 @@ namespace WindowsForm
                 MessageBox.Show("Datos Incorrectos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 txtCorreo.Clear();
                 txtClave.Clear();
-            }
+            }*/
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {//CREO QUE NI HACE FALTA SI TIRO EL MENSAJE DE ERROR, PERO NO SE QUE FORMA ES MEJOR
+            txtCorreo.Clear();//limpio los text box
+            txtClave.Clear();
         }
 
         private List<Usuario> DeserializarUsuariosJSON(string pathArchivo)
         {
-            using (StreamReader streamReader = new StreamReader(pathArchivo))
-            {
-                string jsonUsuarios = streamReader.ReadToEnd();
+            string jsonString = File.ReadAllText(pathArchivo);// lee el contenido del JSON en una cadena
+        
+            List<Usuario> listaUsuarios =  JsonSerializer.Deserialize<List<Usuario>>(jsonString);
 
-                this.usuarios = JsonSerializer.Deserialize<List<Usuario>>(jsonUsuarios);
-            }
-            return this.usuarios;
+            return listaUsuarios;
         }
 
         private bool VerificarUsuario(string correo, string clave)
